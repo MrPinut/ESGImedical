5c62bea6886423bc1b27258da7d133f35c421d47
Anais1210 <azhang@myges.fr>
2023-07-20 23:33:52 +0200

    add table for consultation

--- front-medicare/src/App.tsx
+++ front-medicare/src/App.tsx
@@ -1,24 +1,9 @@
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Button, Web3Modal } from '@web3modal/react'
-import { configureChains, createConfig, WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, polygon } from 'wagmi/chains'
-import HomePage from './pages/Home'
-
-const chains = [arbitrum, mainnet, polygon]
-const projectId = '607c342fe55eeaa0275572f582aa6cd8'
-
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
-  publicClient
-})
-const ethereumClient = new EthereumClient(wagmiConfig, chains)
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-     
+      <Header />
     </>
   )
 }
