fc18a6d1d34170502e81c76583d392c4b945202f
Anais1210 <azhang@myges.fr>
2023-07-20 10:20:30 +0200

    clone template

--- front-medicare/prettier.config.cjs
+++
@@ -1,3 +0,0 @@
-module.exports={
-    plusgins: [require("prettier-plugin-tailwindcss")],
-}
--- front-medicare/src/scenes/home/index.tsx
+++
@@ -1,7 +0,0 @@
-function Home({ accounts, setAccounts }: { accounts: any; setAccounts: any }) {
-  const isConnected = Boolean(accounts[0]);
-
-  return <></>;
-}
-
-export default Home;
