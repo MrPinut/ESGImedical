4e98c360972149e50ba0430e3b6be36dfb4d8579
Anais1210 <azhang@myges.fr>
2023-07-21 19:42:28 +0200

    update patient route and page

--- front-medicare/src/pages/users/Consultations.tsx
+++ front-medicare/src/pages/users/Consultations.tsx
@@ -1,34 +1,58 @@
+import { Card, Col, Row, Container, Button } from "react-bootstrap";
 import Header from "../../components/Header";
-import MiniNav from "../../components/MiniNav";
-import Card from "react-bootstrap/Card";
 
-function Consultation() {
+function DoctorDetailConsultation() {
   return (
     <>
       <Header />
-      <MiniNav />
       <div>
-        <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Consultation</Card.Title>
-            {/* UPDATE DATA FROM BDD OR BLOCKCHAIN WITH CORRESPONDING ID OR ADDRESS */}
-            <Card.Subtitle className="mb-2 text-muted">
-              02/09/2023 à 10:30
-            </Card.Subtitle>
-            <Card.Text>
-              Monsieur Dupuis, n'a pas de baisse de vue par rapport aux années
-              précédentes. <br />
-              Attention à bien protéger ses yeux qui sont sec. Je lui prescrit
-              des gouttes d'eau pour humidifié ses yeux <br />à prendre tous les
-              matins.
-            </Card.Text>
-            {/* put prescriptions link */}
-            <Card.Link href="/ordonnances">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
+        <Button href="/histories">Retour</Button>
       </div>
+
+      <h1 className="text-center p-4">Détail de la consultation</h1>
+      <Card border="dark" className="mt-2" style={{ width: "100%" }}>
+        {/* A REMPLIR AVEC LES DONNEES BDD OU BLOCKCHAIN */}
+
+        <Card.Header>
+          <div>Docteur : Pierre Dupont</div>
+          <div>Spécialité: Généraliste</div>
+          <div>Date : 02/09/2023 à 10:30</div>
+          <div>Lieu: Paris 12ème</div>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title className="p-4">Description</Card.Title>
+          <Card.Text>
+            <div>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+              ratione optio, quidem molestias quos eveniet nemo. Autem iusto,
+              neque repudiandae magni, facere sint deserunt suscipit porro,
+              repellat dolore nisi error!
+            </div>
+          </Card.Text>
+          <Card.Title className="p-4">Résumé</Card.Title>
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique
+            assumenda sunt sequi cupiditate ipsa alias magnam labore inventore
+            corporis. Molestias deleniti officiis quis beatae autem dolorem
+            pariatur ex architecto quisquam.
+          </div>
+          <Card.Title className="p-4">Info urgent, à retenir</Card.Title>
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique
+            assumenda sunt sequi cupiditate ipsa alias magnam labore inventore
+            corporis. Molestias deleniti officiis quis beatae autem dolorem
+            pariatur ex architecto quisquam.
+          </div>
+          <Card.Title className="p-4">Ordonnance prescrit</Card.Title>
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique
+            assumenda sunt sequi cupiditate ipsa alias magnam labore inventore
+            corporis. Molestias deleniti officiis quis beatae autem dolorem
+            pariatur ex architecto quisquam.
+          </div>
+        </Card.Body>
+      </Card>
     </>
   );
 }
-export default Consultation;
+export default DoctorDetailConsultation;
--- front-medicare/src/pages/users/Ordonnance.tsx
+++ front-medicare/src/pages/users/Ordonnance.tsx
@@ -15,34 +15,22 @@ function Ordonnances() {
             <tr>
               <th>#</th>
               <th>Date</th>
-              <th>Docteur</th>
-              <th>Spécialité</th>
-              <th>Description de la consultation</th>
-              <th>Informations importantes à retenir</th>
-              <th>Action</th>
+              <th>Médicament</th>
+              <th>Dose</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>1</td>
               <td>20/07/2023</td>
-              <td>Jean Dupuis</td>
-              <td>Généraliste</td>
-              <td>Consultation pour maux de tête et de gorges</td>
-              <td>Il est juste tombé enrhumé</td>
-              <td>
-                <Button variant="outline-secondary">Détails</Button>{" "}
-              </td>
+              <td>Doliprane</td>
+              <td>1 matin et 1 le soir</td>
             </tr>
             <tr>
               <td>2</td>
               <td>10/06/2023</td>
-              <td>Marie Lefevre</td>
-              <td>Dentiste</td>
-              <td>Check annuel dentaire avec détartrage</td>
-              <td>
-                Attention au dent de sagesses qui doivent bientôt être enlevé
-              </td>
+              <td>Humex</td>
+              <td>1 matin, midi et soir</td>
             </tr>
           </tbody>
         </Table>
