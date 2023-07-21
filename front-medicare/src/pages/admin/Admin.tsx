fe8f5c5b50ef4c6fb08aefe3e236371ae36079dc
Anais1210 <azhang@myges.fr>
2023-07-21 21:04:37 +0200

    add file for admin

--- front-medicare/src/pages/admin/Admin.tsx
+++ front-medicare/src/pages/admin/Admin.tsx
@@ -1,28 +1,11 @@
-import Button from "react-bootstrap/Button";
 import NavAdmin from "../../components/NavAdmin";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import MiniNavAdmin from "../../components/MiniNavAdmin";
 
 function Admin() {
   return (
     <>
       <NavAdmin />
-      <Container className="justify-content-center">
-        <Row>
-          <Col>
-            <Button variant="outline-primary">Listes des patients</Button>{" "}
-          </Col>
-          <Col>
-            <Button variant="outline-primary">Listes des doctors</Button>{" "}
-          </Col>
-          <Col>
-            <Button variant="outline-primary">
-              Listes des parapharmaciens
-            </Button>
-          </Col>
-        </Row>
-      </Container>
+      <MiniNavAdmin />
     </>
   );
 }
